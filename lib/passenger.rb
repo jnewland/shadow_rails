b9c96999dcfb7ef82d863294f6a14e77de393ffc
Jesse Newland <jnewland@gmail.com>
2009-02-05 16:11:41 -0500

    add some requirements, etc

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,46 +1,76 @@
 module Passenger
-  
+
   def apache
-    package "apache2-mpm-worker", :ensure => :installed 
+    package "apache2-mpm-worker", :ensure => :installed
     package "apache2-threaded-dev", :ensure => :installed
-    service :apache2, :require => package "apache2-mpm-worker"
+    service :apache2,
+            :require => [
+              package("apache2-mpm-worker"),
+              package("apache2-threaded-dev")
+            ]
   end
 
   def passenger
-   package "passenger", :ensure => :installed, :provider => :gem
+    package "passenger",
+            :ensure   => :installed,
+            :provider => :gem,
+            :require  => package(:rails)
 
-   version = Gem::SourceIndex.from_installed_gems.find_name("passenger").last.version.to_s
-   path = "/usr/lib/ruby/gems/1.8/gems/passenger-#{version}"
+    path = path_for_gem(passenger)
+    ruby = `which ruby`.chomp
 
-   exec { :build-passenger, :cwd => path, 
-                             :command => '/usr/bin/ruby1.8 -S rake clean apache2', 
-                             :creates => "#{path}/ext/apache2/mod_passenger.so"}
+    exec 'build-passenger',
+         :cwd => path,
+         :command => 'rake clean apache2',
+         :creates => "#{path}/ext/apache2/mod_passenger.so"
+         :require => [
+           package(:passenger),
+           service(:apache)
+         ]
 
-   contents = <<-CONTENTS
+    passenger_module = <<-EOF
      LoadModule passenger_module #{path}/ext/apache2/mod_passenger.so
-   CONTENTS
+    EOF
 
-   file '/etc/apache2/mods-available/passenger.load',
+    file '/etc/apache2/mods-available/passenger.load',
          :ensure   => :present,
-         :contents => contents,
-         :require  => [package(:passenger), exec(:build-passenger)]
-     
-   contents = <<-CONTENTS
+         :contents => passenger_module,
+         :require  => exec('build-passenger')
+
+    passenger_conf = <<-EOF
      PassengerRoot #{path}
-     PassengerRuby /usr/bin/ruby1.8
-   CONTENTS
+     PassengerRuby #{ruby}
+    EOF
 
-   file '/etc/apache2/mods-available/passenger.conf',
+    file '/etc/apache2/mods-available/passenger.conf',
          :ensure   => :present,
-         :contents => contents,
-         :require  => [package(:passenger), exec(:build-passenger)]
+         :contents => passenger_conf,
+         :require  => exec('build-passenger')
 
-   exec {'enable-passenger', :command => 'a2enmod passenger', 
-                             :creates => '/etc/apache2/mods-enabled/passenger.load'}  
- end
+    exec 'enable-passenger',
+         :command => 'a2enmod passenger',
+         :creates => '/etc/apache2/mods-enabled/passenger.load',
+         :require => [
+           file('/etc/apache2/mods-available/passenger.conf'),
+           file('/etc/apache2/mods-available/passenger.load')
+         ],
+         :notify => service(:apache)
+  end
 
+  def rails
+    package "rails", :ensure => :installed, :provider => :gem
+  end
 
- def rails
-   package "rails", :ensure => :installed, :provider => :gem
- end
+protected
+
+  def path_for_gem(gem)
+    begin
+      gemspec = Gem::SourceIndex.from_installed_gems.find_name(gem).last
+      gemspec_path = gemspec.loaded_from
+      rubygems_base = File.join(File.dirname(gemspec_path), '..', 'gems')
+      gem_path = File.join(rubygems_base, (gem+'-'+gemspec.version.to_s))
+    rescue
+      nil
+    end
+  end
 end
